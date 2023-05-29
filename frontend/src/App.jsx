1719a17bf51dce333d883d538f03f439bd8a7a82
saqib <saqib29111@gmail.com>
2023-05-29 17:06:01 +0600

    React Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcom to the E-Commere site</h1>
+    </>
+  )
+}
+
+export default App
