d8a87de2003be9771dcdc5b27adc159accc7404a
saqib <saqib29111@gmail.com>
2023-05-30 12:12:57 +0600

    add HomeScreen, add Product Components, demo products added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Container  } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeSreen from './screens/HomeSreen'
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcom to the E-Commerce site</h1>
+          <HomeSreen />
         </Container>
       </main>
       <Footer/>
