0d28f0be8016cf3f3e5fc161dd75cbe61a148fa6
saqib <saqib29111@gmail.com>
2023-06-14 20:52:59 +0600

    Cart Reducer & Add To Cart Action - create cartReducer, cartAcrtion add cartItems in the browser local storage and Add To Cart Functionality

--- frontend/src/screens/CardScreen.jsx
+++ frontend/src/screens/CardScreen.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useLocation, useParams } from 'react-router-dom'
+import { addToCart } from '../actions/cartActions'
 
 const CardScreen = () => {
+    const params = useParams()
+    const location = useLocation()
+
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    const productId = params.id
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
+
+    console.log(cartItems)
+
+    useEffect(() => {
+        if(productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+    
+
   return (
     <div>
-        Card
+        {qty}
     </div>
   )
 }
