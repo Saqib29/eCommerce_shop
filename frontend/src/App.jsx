87dcd7d978d61fb8decf77e43c721accd9e9124e
saqib <saqib29111@gmail.com>
2023-06-14 16:40:50 +0600

    quantity select & Add to Cart Button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CardScreen from './screens/CardScreen'
 
 const App = () => {
 
@@ -16,6 +17,7 @@ const App = () => {
             <Routes>
               <Route exact path='/' element={<HomeScreen />} />
               <Route path='/product/:id' element={<ProductScreen />} />
+              <Route path='/cart/:id?' element={ <CardScreen /> } />
 
             </Routes>
           </Container>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
-import ReactDOM from 'react-dom';
-import ReactImageZoom from 'react-image-zoom'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { listProductDetails } from '../actions/productActions';
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
-const ProductScreen = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
+const ProductScreen = ({ history, match }) => {
+  const [qty, setQty] = useState(0)
+
+  const params = useParams()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    dispatch(listProductDetails(id));
-  }, []);
+    dispatch(listProductDetails(params.id));
+  }, [dispatch, params]);
+
+  const addToCardHandler = () => {
+    navigate(`/cart/${params.id}?qty=${qty}`)
+  }
 
   return (
     <>
@@ -73,8 +78,41 @@ const ProductScreen = () => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                
+                { product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qunatity</Col>
+                      <Col>
+                        <Form.Control as='select' value={qty} onChange={(e) => setQty(e.target.value)}
+                        
+                          style={{
+                            backgroundColor: '#272B30',
+                            color: '#fff',
+                            fontSize: '0.8rem',
+                            padding: '0.3rem',
+                            appearance: 'none',
+                            border: '#141619',
+                            backgroundImage: 'url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2212%22%20height%3D%228%22%20viewBox%3D%220%200%2012%208%22%3E%3Cpath%20d%3D%22M1%201L6%206%2011%201%22%20fill%3D%22none%22%20stroke%3D%22%23fff%22%20stroke-width%3D%222%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%2F%3E%3C%2Fsvg%3E")',
+                            backgroundPosition: `right calc(0.39em + 7px) center`,
+                            backgroundRepeat: 'no-repeat',
+                            // backgroundPosition: 'right center',
+                            backgroundSize: '12px',
+                            paddingRight: '15px',
+                          }}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}> { x + 1 } </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                ) }
+
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCardHandler}
                     variant="success"
                     className="btn-block w-100"
                     type="button"
