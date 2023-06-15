abc5b8e3d5c83c11cbf6d2faa5268c7801b16e13
saqib <saqib29111@gmail.com>
2023-06-16 02:59:11 +0600

    User Registration & Password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asycnHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import generatToken from '../utils/generateToken.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -26,6 +27,41 @@ const authUser = asycnHandler(async (req, res) => {
 })
 
 
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asycnHandler(async (req, res) => {
+    const { name, email, password } = req.body
+    
+    const userExists = await User.findOne({ email })
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+    
+})
+
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
@@ -46,4 +82,4 @@ const getUserProfile = asycnHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
